02cb3887ff8d624aa6f90dc7b18704eddaf718d9
huangdaxianer <leeoohs@sinq.com>
2020-04-09 22:08:39 +0800

    更改注释

--- course_4.1/magicians_4.1.2.py
+++ course_4.1/magicians_4.1.2.py
@@ -9,8 +9,9 @@ for magician in magicians:
 这个循环第二次迭代时变量magician的值为david，因此python打印的第一条消息的抬头为David，
 这个循环第三次迭代时变量magician的值为carolina，因此python打印的第一条消息的抬头为Carolina。'''
 
-# 在for循环中，想包含多少行代码都可以。在代码行for magician in magicians后面，每个缩进的代码行都是循环的一部分，且将针对列表中的每个值都执行一次
+# 在for循环中，想包含多少行代码都可以。
 magicians = ['alice','david','carolina']
+# 在代码行for magician in magicians后面，每个缩进的代码行都是循环的一部分，且将针对列表中的每个值都执行一次
 for magician in magicians:
     print(magician.title() + ",that was a great trick!")
     print("I can't wait to see your next trick," + magician.title() + ".\n")
