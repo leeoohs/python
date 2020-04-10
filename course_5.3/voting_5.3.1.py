541e64f9206809dc9c5cf3dc851947644b0e4573
huangdaxianer <leeoohs@sinq.com>
2020-04-10 22:15:46 +0800

    2020.4.10

---
+++ course_5.3/voting_5.3.1.py
@@ -0,0 +1,8 @@
+# 如果条件测试的结果为True，python就会执行紧跟在if语句后面的代码，否则忽略
+
+# 检查一个人是否够投票年龄
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+    # if语句中，缩进的作用于for循环中的相同。
+    print("Have you registered to vote yet?")
