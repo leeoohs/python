417e0ba5e8c07cc906f6dbbdd211c13a372326ba
huangdaxianer <leeoohs@sinq.com>
2020-04-10 14:05:25 +0800

    2020.4.10更改代码

--- course_4.2/pizza_case_4-1.py
+++ course_4.2/pizza_case_4-1.py
@@ -1,15 +1,15 @@
 # 想出至少3种你喜欢的pizza，并用for循环将pizza名字都打印出来
-pizzas = {'seafood pizza', 'beef pizza', 'corn pizza'}
+pizzas = ['seafood pizza', 'beef pizza', 'corn pizza']
 for pizza in pizzas:
     print(pizza)
 
 # 对每一种pizza都显示一行输出，如"I like peperoni pizza"
-pizzas = {'seafood pizza', 'beef pizza', 'corn pizza'}
+pizzas = ['seafood pizza', 'beef pizza', 'corn pizza']
 for pizza in pizzas:
     print("I like " + pizza + ".")
 
 # 在for循环结尾输出一个总结性语句，如："I really love pizza！"
-pizzas = {'seafood pizza', 'beef pizza', 'corn pizza'}
+pizzas = ['seafood pizza', 'beef pizza', 'corn pizza']
 for pizza in pizzas:
     print("I like " + pizza + ".")
 
