978e87aa89c9f85b3ffab2624c0249a910e1bb4e
huangdaxianer <leeoohs@sinq.com>
2020-04-13 17:18:50 +0800

    修改代码

--- course_6.2/numbers_case_6-2.py
+++ course_6.2/numbers_case_6-2.py
@@ -1,11 +1,12 @@
 # 喜欢的数字
 # 使用字典来存储一些人喜欢的数字，共5人
-favorite_numbers = {'liukai': 3,
-                    'lixiaohua': 6,
-                    'lishaoyan': 7,
-                    'xiaohai': 8,
-                    'daxian': 7,
-                    }
+favorite_numbers = {
+    'liukai': 3,
+    'lixiaohua': 6,
+    'lishaoyan': 7,
+    'xiaohai': 8,
+    'daxian': 7,
+    }
 print("Liukai favorite number is " + str(favorite_numbers['liukai']) + ".")
 print("Lixiaohua favorite number is " + str(favorite_numbers['lixiaohua']) + ".")
 print("Lishaoyan favorite number is " + str(favorite_numbers['lishaoyan']) + ".")
--- course_6.2/vocabulary_case_6-3.py
+++ course_6.2/vocabulary_case_6-3.py
@@ -1,10 +1,11 @@
 # 词汇表
-vocabularys = {'for': '循环',
-               'if': '条件测试',
-               'elif': '另一个if',
-               'print': '打印',
-               'del': '删除',
-               }
+vocabularys = {
+    'for': '循环',
+    'if': '条件测试',
+    'elif': '另一个if',
+    'print': '打印',
+    'del': '删除',
+    }
 print("For:" + "\n" + vocabularys['for'])
 print("If:" + "\n" + vocabularys['if'])
 print("Elif:" + "\n" + vocabularys['elif'])
